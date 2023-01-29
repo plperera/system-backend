c0e0d8be92d81c2e3f165a9419cef70c7c36e707
Pedro Pereira <plp.leoncio@gmail.com>
2023-01-28 21:50:50 -0300

    feat: testes para rota POST /ordder

---
+++ tests/factories/ordder-factory.ts
@@ -0,0 +1,122 @@
+import bcrypt from "bcrypt";
+import faker from "@faker-js/faker";
+import { prisma } from "@/config";
+import { users } from ".prisma/client";
+import { signUpBody } from "./auth-factory";
+import productFactory from "./products-factory";
+import clientFactory from "./clients-factory";
+import addressFactory from "./address-factory";
+
+export type newItemBody = {
+    ordderId: number,
+    productId: number,
+    itemAmount: number,
+    itemPrice: number
+}
+
+export type newOrdderBody = {
+    userId: number,
+    clientId: number,
+    addressId: number
+}
+
+export type fullOrdderBody = {
+    userId: number,
+    numberOfItens: number
+}
+
+async function createValidOrdderBody(userId: number) {
+
+    const client = await clientFactory.createClient(await clientFactory.createClientBody())
+
+    const address = await addressFactory.createAddress(await addressFactory.generateAddressValidBody(client.id))
+
+    return {
+        userId,
+        clientId:client.id,
+        addressId:address.id
+    }
+}
+
+async function createValidOrdder(body: newOrdderBody) {
+    return prisma.ordder.create({
+        data:{
+            userId: body.userId,
+            clientId: body.clientId,
+            addressId: body.addressId,
+        }
+    })
+}
+
+async function createValidBodyItem(body: { ordderId: number, productId: number}) {
+    return{
+        ordderId: body.ordderId,
+        productId: body.productId,
+        itemAmount: Number(faker.random.numeric(2)) * 10,
+        itemPrice: Number(faker.random.numeric(2)) * 10,
+    }
+}
+
+async function createValidBodyItemWithoutOrdderId(body: { productId: number}) {
+    return{
+        productId: body.productId,
+        itemAmount: Number(faker.random.numeric(2)) * 10,
+        itemPrice: Number(faker.random.numeric(2)) * 10,
+    }
+}
+
+async function createValidItem(body: newItemBody) {
+    return prisma.ordderItem.create({
+        data:{
+            ordderId: body.ordderId,
+            productId: body.productId,
+            itemAmount: body.itemAmount,
+            itemPrice: body.itemPrice,
+        }
+    })
+}
+
+async function createFullOrdder(body: fullOrdderBody) {
+
+    const ordder = await ordderFactory.createValidOrdder(await ordderFactory.createValidOrdderBody(body.userId))
+
+    const productIdArray: any = []
+
+    for (let i = body.numberOfItens; i !== 0; i--){
+
+        const newProduct = await productFactory.createProduct(await productFactory.generateProductValidBody())
+        productIdArray.push( newProduct.id )
+
+    }
+
+    productIdArray.map(async (e: number) => await ordderFactory.createValidBodyItem({ordderId: ordder.id, productId: e}))
+
+    prisma.ordderItem.createMany({
+        data:[productIdArray]
+    })
+    
+}
+
+async function getOrdderById(ordderId: number) {
+    return prisma.ordder.findFirst({
+        where:{
+            id: ordderId
+        },
+        include: {
+            ordderItem: true
+        }
+    })
+}
+
+
+const ordderFactory = {
+    createValidOrdderBody,
+    createValidOrdder,
+    createValidBodyItem,
+    createValidItem,
+    createFullOrdder,
+    createValidBodyItemWithoutOrdderId,
+    getOrdderById
+}
+
+export default ordderFactory
---
+++ tests/integration/ordder.test.ts
@@ -0,0 +1,403 @@
+import app, { init } from "@/app";
+import { prisma } from "@/config";
+import { faker } from "@faker-js/faker";
+import dayjs from "dayjs";
+import httpStatus from "http-status";
+import supertest from "supertest";
+import authFactory from "../factories/auth-factory";
+import userFactory from "../factories/user-factory";
+import clientFactory from "../factories/clients-factory";
+import { cleanDb, generateClientWithAddress, generateValidToken } from "../helpers";
+import * as jwt from "jsonwebtoken";
+import addressFactory from "../factories/address-factory";
+import productFactory from "../factories/products-factory";
+import ordderFactory from "../factories/ordder-factory";
+ 
+
+beforeAll(async () => {
+  await init();
+  await cleanDb();
+});
+
+beforeEach(async () => {
+  await cleanDb();
+});
+
+
+const server = supertest(app);
+
+describe("POST /ordder", () => {
+
+    it("should respond with status 401 if no token is given", async () => {
+
+        const response = await server.post("/ordder");
+
+        expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+
+    });
+
+    it("should respond with status 401 if given token is not valid", async () => {
+
+        const token = faker.lorem.word();
+
+        const response = await server.post("/ordder").set("Authorization", `Bearer ${token}`);
+
+        expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+
+    });
+
+    it("should respond with status 401 if there is no session for given token", async () => {
+
+        const body = authFactory.generateValidBody()
+        const user = await userFactory.createUser(body);
+        
+        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
+
+        const response = await server.post("/ordder").set("Authorization", `Bearer ${token}`);
+
+        expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    describe("when token is valid and user have a session", () => {
+
+        it("should respond with status 400 when body is blank", async () => {
+
+            const token = await generateValidToken()
+
+            const body = {}
+
+            const response = await server.post("/ordder").set("Authorization", `Bearer ${token}`).send(body)
+
+            expect(response.status).toBe(httpStatus.BAD_REQUEST);
+        });
+
+        it("should respond with status 400 when body doenst have itens", async () => {
+
+            const token = await generateValidToken()
+
+            const user = await userFactory.getFirstUser()
+
+            const ordderBody = ordderFactory.createValidOrdderBody(user.id)
+
+            const response = await server.post("/ordder").set("Authorization", `Bearer ${token}`).send(ordderBody)
+
+            expect(response.status).toBe(httpStatus.BAD_REQUEST);
+
+        });
+
+        it("should respond with status 400 when body have itens butt is empty", async () => {
+
+            const token = await generateValidToken()
+
+            const user = await userFactory.getFirstUser()
+
+            const ordderBody = ordderFactory.createValidOrdderBody(user.id)
+
+            const body = {...ordderBody, itens:[{}]}
+
+            const response = await server.post("/ordder").set("Authorization", `Bearer ${token}`).send(body)
+
+            expect(response.status).toBe(httpStatus.BAD_REQUEST);
+
+        });
+        
+        describe("when body is valid", async () => {
+
+            it("should respond with status 404 when client not exist", async () => {
+
+                const token = await generateValidToken()
+
+                const user = await userFactory.getFirstUser()
+
+                const ordderBody = await ordderFactory.createValidOrdderBody(user.id)
+
+                const product = await productFactory.createProduct(await productFactory.generateProductValidBody())
+
+                ordderBody.clientId = 99999999999
+
+                const body = {
+                    ...ordderBody, 
+                    itens:[ await ordderFactory.createValidBodyItemWithoutOrdderId({ productId: product.id }) ]
+                }
+
+                const response = await server.post("/ordder").set("Authorization", `Bearer ${token}`).send(body)
+
+                expect(response.status).toBe(httpStatus.NOT_FOUND);
+            })
+
+            it("should respond with status 404 when address not exist", async () => {
+
+                const token = await generateValidToken()
+
+                const user = await userFactory.getFirstUser()
+
+                const ordderBody = await ordderFactory.createValidOrdderBody(user.id)
+
+                const product = await productFactory.createProduct(await productFactory.generateProductValidBody())
+
+                ordderBody.addressId = 99999999999
+
+                const body = {
+                    ...ordderBody, 
+                    itens:[ await ordderFactory.createValidBodyItemWithoutOrdderId({ productId: product.id }) ]
+                }
+
+                const response = await server.post("/ordder").set("Authorization", `Bearer ${token}`).send(body)
+
+                expect(response.status).toBe(httpStatus.NOT_FOUND);
+            })
+
+            it("should respond with status 404 when product not exist", async () => {
+
+                const token = await generateValidToken()
+
+                const user = await userFactory.getFirstUser()
+
+                const ordderBody = await ordderFactory.createValidOrdderBody(user.id)
+
+                //const product = await productFactory.createProduct(await productFactory.generateProductValidBody())
+
+                const body = {
+                    ...ordderBody, 
+                    itens:[ await ordderFactory.createValidBodyItemWithoutOrdderId({ productId: 99999999 }) ]
+                }
+
+                const response = await server.post("/ordder").set("Authorization", `Bearer ${token}`).send(body)
+
+                expect(response.status).toBe(httpStatus.NOT_FOUND);
+            })
+
+            it("should respond with status 403 when product not exist", async () => {
+
+                const token = await generateValidToken()
+
+                const user = await userFactory.getFirstUser()
+
+                const ordderBody = await ordderFactory.createValidOrdderBody(user.id)
+
+                const product = await productFactory.createProduct(await productFactory.generateProductValidBody())
+
+                const secondClient = await clientFactory.createClient( await clientFactory.createClientBody())
+
+                const secondAddress = await addressFactory.createAddress( await addressFactory.generateAddressValidBody(secondClient.id))
+
+                ordderBody.addressId = secondAddress.id
+
+                const body = {
+                    ...ordderBody, 
+                    itens:[ await ordderFactory.createValidBodyItemWithoutOrdderId({ productId: product.id }) ]
+                }
+
+                const response = await server.post("/ordder").set("Authorization", `Bearer ${token}`).send(body)
+
+                expect(response.status).toBe(httpStatus.FORBIDDEN);
+            })
+
+            describe("when body data is valid and have 1 ordderItem", async () => {
+
+                it("should respond with status 201", async () => {
+
+                    const token = await generateValidToken()
+
+                    const user = await userFactory.getFirstUser()
+
+                    const ordderBody = await ordderFactory.createValidOrdderBody(user.id)
+
+                    const productArray = []
+
+                    for (let i = 1; i <= 1; i++){
+
+                        const product = await productFactory.createProduct(await productFactory.generateProductValidBody())
+
+                        const ordderItem = await ordderFactory.createValidBodyItemWithoutOrdderId({productId: product.id})
+
+                        productArray.push(ordderItem)
+
+                    }
+                    
+                    const body = {
+                        ...ordderBody, 
+                        itens: productArray
+                    }
+
+                    const response = await server.post("/ordder").set("Authorization", `Bearer ${token}`).send(body)
+
+                    expect(response.status).toBe(httpStatus.CREATED);
+
+                })
+
+                it("should respond with ordder data", async () => {
+
+                    const token = await generateValidToken()
+
+                    const user = await userFactory.getFirstUser()
+
+                    const ordderBody = await ordderFactory.createValidOrdderBody(user.id)
+
+                    const productArray = []
+
+                    for (let i = 1; i <= 1; i++){
+
+                        const product = await productFactory.createProduct(await productFactory.generateProductValidBody())
+
+                        const ordderItem = await ordderFactory.createValidBodyItemWithoutOrdderId({productId: product.id})
+
+                        productArray.push(ordderItem)
+
+                    }
+                    
+                    const body = {
+                        ...ordderBody, 
+                        itens: productArray
+                    }
+
+                    const response = await server.post("/ordder").set("Authorization", `Bearer ${token}`).send(body)
+
+                    const ordderData = await ordderFactory.getOrdderById(response.body.id)
+
+                    expect(response.body).toMatchObject(ordderData);
+
+                })
+
+                describe("when body data is valid and have 2 ordderItem", async () => {
+
+                    it("should respond with status 201", async () => {
+    
+                        const token = await generateValidToken()
+    
+                        const user = await userFactory.getFirstUser()
+    
+                        const ordderBody = await ordderFactory.createValidOrdderBody(user.id)
+    
+                        const productArray = []
+    
+                        for (let i = 1; i <= 2; i++){
+    
+                            const product = await productFactory.createProduct(await productFactory.generateProductValidBody())
+    
+                            const ordderItem = await ordderFactory.createValidBodyItemWithoutOrdderId({productId: product.id})
+    
+                            productArray.push(ordderItem)
+    
+                        }
+                        
+                        const body = {
+                            ...ordderBody, 
+                            itens: productArray
+                        }
+    
+                        const response = await server.post("/ordder").set("Authorization", `Bearer ${token}`).send(body)
+    
+                        expect(response.status).toBe(httpStatus.CREATED);
+    
+                    })
+    
+                    it("should respond with ordder data", async () => {
+    
+                        const token = await generateValidToken()
+    
+                        const user = await userFactory.getFirstUser()
+    
+                        const ordderBody = await ordderFactory.createValidOrdderBody(user.id)
+    
+                        const productArray = []
+    
+                        for (let i = 1; i <= 2; i++){
+    
+                            const product = await productFactory.createProduct(await productFactory.generateProductValidBody())
+    
+                            const ordderItem = await ordderFactory.createValidBodyItemWithoutOrdderId({productId: product.id})
+    
+                            productArray.push(ordderItem)
+    
+                        }
+                        
+                        const body = {
+                            ...ordderBody, 
+                            itens: productArray
+                        }
+    
+                        const response = await server.post("/ordder").set("Authorization", `Bearer ${token}`).send(body)
+    
+                        const ordderData = await ordderFactory.getOrdderById(response.body.id)
+    
+                        expect(response.body).toMatchObject(ordderData);
+    
+                    })
+
+                    describe("when body data is valid and have 15 ordderItem", async () => {
+
+                        it("should respond with status 201", async () => {
+        
+                            const token = await generateValidToken()
+        
+                            const user = await userFactory.getFirstUser()
+        
+                            const ordderBody = await ordderFactory.createValidOrdderBody(user.id)
+        
+                            const productArray = []
+        
+                            for (let i = 1; i <= 15; i++){
+        
+                                const product = await productFactory.createProduct(await productFactory.generateProductValidBody())
+        
+                                const ordderItem = await ordderFactory.createValidBodyItemWithoutOrdderId({productId: product.id})
+        
+                                productArray.push(ordderItem)
+        
+                            }
+                            
+                            const body = {
+                                ...ordderBody, 
+                                itens: productArray
+                            }
+        
+                            const response = await server.post("/ordder").set("Authorization", `Bearer ${token}`).send(body)
+        
+                            expect(response.status).toBe(httpStatus.CREATED);
+        
+                        })
+        
+                        it("should respond with ordder data", async () => {
+        
+                            const token = await generateValidToken()
+        
+                            const user = await userFactory.getFirstUser()
+        
+                            const ordderBody = await ordderFactory.createValidOrdderBody(user.id)
+        
+                            const productArray = []
+        
+                            for (let i = 1; i <= 15; i++){
+        
+                                const product = await productFactory.createProduct(await productFactory.generateProductValidBody())
+        
+                                const ordderItem = await ordderFactory.createValidBodyItemWithoutOrdderId({productId: product.id})
+        
+                                productArray.push(ordderItem)
+        
+                            }
+                            
+                            const body = {
+                                ...ordderBody, 
+                                itens: productArray
+                            }
+        
+                            const response = await server.post("/ordder").set("Authorization", `Bearer ${token}`).send(body)
+        
+                            const ordderData = await ordderFactory.getOrdderById(response.body.id)
+        
+                            expect(response.body).toMatchObject(ordderData);
+        
+                        })
+        
+                    })
+    
+                })
+
+            })
+
+        }) 
+        
+    })
+});
